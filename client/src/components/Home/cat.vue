362b562e33312b14655115314830082057b129d4
NdungutseCharles103 <ndungutsecharles103@gmail.com>
2022-04-20 10:46:48 +0200

    still cali

--- client/src/components/Home/cat.vue
+++ client/src/components/Home/cat.vue
@@ -2,48 +2,48 @@
     <div class="categories">
         <h2>Categories</h2>
         <div class="cat-cont">
-            <a href="Action.html" class="cat-card">
+            <router-link to="/action" class="cat-card">
                 <div class="desc">
                     <p>Action</p>
                     <div class="trends__btn"><p>Explore</p></div>
                 </div>
 
-            </a>
-            <a href="Romance.html" class="cat-card">
+            </router-link>
+            <router-link to="/romance" class="cat-card">
                 <div class="desc">
                     <p>Romance</p>
                     <div class="trends__btn"><p>Explore</p></div>
                 </div>
 
-            </a>
-            <a href="Adventure.html" class="cat-card">
+            </router-link>
+            <router-link to="/adventure" class="cat-card">
                 <div class="desc">
                     <p>Adventures</p>
                     <div class="trends__btn"><p>Explore</p></div>
                 </div>
 
-            </a>
-            <a href="Comedy.html" class="cat-card">
+            </router-link>
+            <router-link to="/comedy" class="cat-card">
                 <div class="desc">
                     <p>Comedy</p>
                     <div class="trends__btn"><p>Explore</p></div>
                 </div>
 
-            </a>
-            <a href="Animation.html" class="cat-card">
+            </router-link>
+            <router-link to="/animation" class="cat-card">
                 <div class="desc">
                     <p>Animation</p>
                     <div class="trends__btn"><p>Explore</p></div>
                 </div>
 
-            </a>
-            <a href="News.html" class="cat-card">
+            </router-link>
+            <router-link to="/news" class="cat-card">
                 <div class="desc">
                     <p>Other</p>
                     <div class="trends__btn"><p>Explore</p></div>
                 </div>
 
-            </a>
+            </router-link>
         </div>
     </div>
 </template>
