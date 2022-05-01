74abd6693d02baf3f0254a4529b2fef6207daba8
NdungutseCharles103 <ndungutsecharles103@gmail.com>
2022-05-01 21:03:39 +0200

    __

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors');
-require('dotenv').config()
+// require('dotenv').config()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
