402f4e21438205e169ef1222bac296ef68d72b5c
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-06-01 21:35:57 +0200

    vue pagination

--- client/src/components/store/MyStore.vue
+++ client/src/components/store/MyStore.vue
@@ -1,12 +1,17 @@
 <template>
     <div class="container-fluid">
-        
+        <MyNavbar />
     </div>
 </template>
 
 <script>
+import MyNavbar from '../Navbar.vue'
+
 export default {
     name: "MyStore",
+    components: {
+        MyNavbar,
+    },
     data() {
         return {
             movies: [],
