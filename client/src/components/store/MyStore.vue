0478bf0d693dbabe78994ed99621a281cb812066
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-06-04 16:51:24 +0200

    some changes

--- client/src/components/store/MyStore.vue
+++ client/src/components/store/MyStore.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="container-fluid">
+        <my-header />
         <MyNavbar />
     </div>
 </template>
 
 <script>
 import MyNavbar from '../Navbar.vue'
+import myHeader from '../Header.vue'
 
 export default {
     name: "MyStore",
     components: {
         MyNavbar,
+        myHeader
     },
     data() {
         return {
